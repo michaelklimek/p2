e3a3411834c8ae522e230bd3d43d635b18dd19d3
Michael Klimek <michaelklimek@g.harvard.edu>
2014-10-02 00:13:41 -0500

    Adding index and css files and folder

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>xkcd Password Generator</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="title">
+		<h1>xkcd Password Generator</h1>
+		<h2 class="pass">hello-to-the-whole-wide-world</h2>
+	</div>
+	<div>
+		<form action="#" method="GET">
+			<ul class="questions">
+				Number of Words<input type="text">&#40;Max 9&#41;<br>
+				<input type="checkbox">Add a number&#63;<br>
+				<input type="checkbox">Add a symbol&#63;<br>
+				<input type="submit" value="Generate Password">
+			</ul>
+		</form>
+	</div>
+</body>
+</html>
