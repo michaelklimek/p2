3b71132d76d2162650ba0a2e3b11d350023abc2e
Michael Klimek <michaelklimek@g.harvard.edu>
2014-10-02 01:27:05 -0500

    Adding Logic

---
+++ list.php
@@ -0,0 +1,25 @@
+<?php
+
+$words = Array(
+0 => 'tree',
+1 => 'goat',
+2 => 'cow',
+3 => 'chicken',
+4 => 'horse',
+);
+
+$symbols = Array(
+0 => '&amp;',
+1 => '&#63;',
+2 => '&#35;',
+3 => '&#36;',
+4 => '&#33;',
+);
+
+$number = "";
+for ($i=0; $i <= 9; $i++) { 
+ 	$numbers = Array(
+ 		$i => '$i',);
+ } 
+
+ print_r($numbers);
--- logic.php
+++ logic.php
@@ -1,5 +1,7 @@
 <?php
 
+require "list.php";
+
 
 
 
